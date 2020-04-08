489c2c3c8f59fd926c84ffaacf88a44c2bed2a00
Jie Yan <jieyan620@gmail.com>
2020-04-08 00:42:31 -0700

    add more files

--- client/src/App.js
+++ client/src/App.js
@@ -12,79 +12,56 @@ const App = () => {
   })
 
   const handleInputChange = ({ target }) => {
-    setItemState({ ...itemState, [target.name]: target.value })
+    setBookState({ ...bookState, [target.name]: target.value })
   }
 
-  const handleCreateBook = event => {
-    console.log(event)
-    event.preventDefault()
-    Item.create({
-      text: itemState.text,
-      isDone: false
-    })
-      .then(({ data: item }) => {
-        let items = JSON.parse(JSON.stringify(itemState.items))
-        items.push(item)
-        setItemState({ ...itemState, items, text: '' })
-      })
-  }
+  // const handleCreateBook = event => {
+  //   console.log(event)
+  //   event.preventDefault()
+  //   Item.create({
+  //     text: itemState.text,
+  //     isDone: false
+  //   })
+  //     .then(({ data: item }) => {
+  //       let items = JSON.parse(JSON.stringify(itemState.items))
+  //       items.push(item)
+  //       setItemState({ ...itemState, items, text: '' })
+  //     })
+  // }
 
-  const handleToggleComplete = (id, isDone) => {
-    Item.update(id, { isDone: !isDone })
-      .then(() => {
-        let items = JSON.parse(JSON.stringify(itemState.items))
-        items.forEach(item => {
-          if (item._id === id) {
-            item.isDone = !isDone
-          }
-        })
-        setItemState({ ...itemState, items })
-      })
-  }
+  // const handleToggleComplete = (id, isDone) => {
+  //   Item.update(id, { isDone: !isDone })
+  //     .then(() => {
+  //       let items = JSON.parse(JSON.stringify(itemState.items))
+  //       items.forEach(item => {
+  //         if (item._id === id) {
+  //           item.isDone = !isDone
+  //         }
+  //       })
+  //       setItemState({ ...itemState, items })
+  //     })
+  // }
 
-  const handleDeleteItem = id => {
-    Item.delete(id)
-      .then(() => {
-        let items = JSON.parse(JSON.stringify(itemState.items))
-        items = items.filter(item => item._id !== id)
-        setItemState({ ...itemState, items })
-      })
-  }
+  // const handleDeleteItem = id => {
+  //   Item.delete(id)
+  //     .then(() => {
+  //       let items = JSON.parse(JSON.stringify(itemState.items))
+  //       items = items.filter(item => item._id !== id)
+  //       setItemState({ ...itemState, items })
+  //     })
+  // }
 
   useEffect(() => {
-    Item.read()
-      .then(({ data: items }) => {
-        setItemState({ ...itemState, items })
+    Book.read()
+      .then(({ data: books }) => {
+        setBookState({ ...bookState, books })
       })
   }, [])
 
   return (
     <div>
 
-      <form>
-        <p>
-          <label htmlFor="text">item</label>
-          <input
-            type="text"
-            name="text"
-            value={itemState.text}
-            onChange={handleInputChange} />
-        </p>
-        <p>
-          <button color="primary" onClick={handleCreateItem}>Add Item</button>
-        </p>
-      </form>
-      <ul>
-        {
-          itemState.items.map(({ _id, text, isDone }) => (
-            <li key={_id}>
-              {text}
-              <button color={isDone ? 'success' : 'warning'} onClick={() => handleToggleComplete(_id, isDone)}>{isDone ? 'complete' : 'incomplete'}</button>
-              <button color="danger" onClick={() => handleDeleteItem(_id)}>delete</button>
-            </li>
-          ))
-        }
-      </ul>
+      
     </div>
   )
 }
